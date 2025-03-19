672378a0b4aa9986e45e093b2e25675ce44c15f7
Joe Krisciunas <94765236+krisciu@users.noreply.github.com>
2025-03-18 22:36:22 -0700

    swap api Base url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // Backend API configuration
 const config = {
   // API Base URL - Change this to your production backend URL when deploying
-  apiBaseUrl: 'http://34.168.213.229:3000',
+  apiBaseUrl: 'https://deface-api-851374168186.us-west1.run.app',
 };
 
 export default config; 
